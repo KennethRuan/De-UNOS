75654d2ce59f20c08dea0f7caa2a55a94ef7eb8b
KennethRuan <kennethjruan@gmail.com>
2024-02-18 03:22:24 -0800

    Fleshed out backend with data schemas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { connectToGateway } from "./gateway";
-import { createAsset, getAllAssets } from "./chaincode";
+import { createDonor, createPatient, getAllDonors, getAllPatients, getDonor, getPatient, } from "./chaincode";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(express.json());
@@ -14,9 +15,11 @@ app.listen(PORT, () => {
 // Hyperledger Fabric API Endpoints to hit SDK
 app.post("/api/add-patient", async (req: Request, res: Response) => {
   res.send("Added Patient");
+  const patientId = uuidv4()
+  const { age, height, bloodType, pediatricStatus, location, meldScore, hlaB27Antibodies } = req.body;
   const { client, gateway, network, contract } = await connectToGateway();
   try {
-    await createAsset(contract, `asset${Date.now()}`);
+    await createPatient(contract, patientId, age, height, bloodType, pediatricStatus, location, meldScore, hlaB27Antibodies);
   } finally {
     gateway.close();
     client.close();
@@ -27,7 +30,7 @@ app.get("/api/query-all-patients", async (req: Request, res: Response) => {
   res.send("Query All Patients");
   const { client, gateway, network, contract } = await connectToGateway();
   try {
-    const data = await getAllAssets(contract);
+    const data = await getAllPatients(contract);
     console.log("Success", data);
     res.send(data);
   } finally {
@@ -36,6 +39,67 @@ app.get("/api/query-all-patients", async (req: Request, res: Response) => {
   }
 });
 
-app.get("/api/query-patient/:id", (req: Request, res: Response) => {
+app.get("/api/query-patient/:id", async (req: Request, res: Response) => {
   res.send("Query Patient");
+  const patientId = req.params.id;
+  const { client, gateway, network, contract } = await connectToGateway();
+  try {
+    const data = await getPatient(contract, patientId);
+    console.log("Success", data);
+    res.send(data);
+  } finally {
+    gateway.close();
+    client.close();
+  }
+});
+
+app.post("/api/add-donor", async (req: Request, res: Response) => {
+  res.send("Added Donor");
+  const donationId = uuidv4()
+  const { organ, age, height, bloodType, pediatricStatus, location, hlaB27AntigenTest } = req.body;
+  const { client, gateway, network, contract } = await connectToGateway();
+  try {
+    await createDonor(contract, donationId, organ, age, height, bloodType, pediatricStatus, location, hlaB27AntigenTest);
+  } finally {
+    gateway.close();
+    client.close();
+  }
+});
+
+app.get("/api/query-all-donors", async (req: Request, res: Response) => {
+  res.send("Query All Donors");
+  const { client, gateway, network, contract } = await connectToGateway();
+  try {
+    const data = await getAllDonors(contract);
+    console.log("Success", data);
+    res.send(data);
+  } finally {
+    gateway.close();
+    client.close();
+  }
+});
+
+app.get("/api/query-donor/:id", async (req: Request, res: Response) => {
+  res.send("Query Donor");
+  const donorId = req.params.id;
+  const { client, gateway, network, contract } = await connectToGateway();
+  try {
+    const data = await getDonor(contract, donorId);
+    console.log("Success", data);
+    res.send(data);
+  } finally {
+    gateway.close();
+    client.close();
+  }
+});
+
+app.get("/api/add-donation", async (req: Request, res: Response) => {
+  res.send("Added Donation");
+  const { client, gateway, network, contract } = await connectToGateway();
+  try {
+    // await createDonation(contract, donationId, organ, age, height, bloodType, pediatricStatus, location, hlaB27AntigenTest);
+  } finally {
+    gateway.close();
+    client.close();
+  }
 });
