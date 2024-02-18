248087e680fac365df7cc6e8980077df9f796f61
Roselyn Huynh <roselynh100@gmail.com>
2024-02-18 01:20:31 -0800

    Add organ requested for patient registering

--- frontend/frontend/pages/register_patient.py
+++ frontend/frontend/pages/register_patient.py
@@ -5,9 +5,13 @@ from frontend.data.patient_data import data
 
 class RegisterState(rx.State):
     searched: bool = False
+    organ_inputted: bool = False
 
     def search(self):
         self.searched = True
+
+    def input_organ(self):
+        self.organ_inputted = True
 
 
 def patient_table(data):
@@ -19,38 +23,106 @@ def patient_table(data):
 
 def register_patient():
     return internal_template(
-        rx.box(
+        rx.scroll_area(
             rx.heading("Register a patient", margin_bottom="36px"),
             rx.tooltip(
-                rx.text("Patient ID", cursor="default"),
+                rx.text("Please input the Patient ID.", cursor="default"),
                 content="This is the patient's identifier, like a hospital record number.",
                 align="start",
                 delay_duration=300,
             ),
             rx.hstack(
                 rx.box(
-                    rx.input(name="patient_id", variant="surface", size="2"),
+                    rx.input(
+                        name="patient_id",
+                        variant="surface",
+                        size="2",
+                        disabled=rx.cond(RegisterState.searched == True, True, False),
+                        cursor=rx.cond(
+                            RegisterState.searched == True, "not-allowed", "auto"
+                        ),
+                    ),
                     width="50%",
                 ),
-                rx.button("Search", on_click=RegisterState.search),
+                rx.button(
+                    "Search",
+                    on_click=RegisterState.search,
+                    disabled=rx.cond(RegisterState.searched == True, True, False),
+                ),
             ),
             rx.cond(
                 RegisterState.searched,
                 # TODO: add a delay / loading state (animation)
-                rx.vstack(
+                rx.box(
                     rx.text(
-                        "Patient data found. The following information will be uploaded to our secure blockchain network. Please review patient vitals before proceeding."
+                        "Patient data found. Please input what organ is in need.",
+                        margin_top="24px",
                     ),
-                    rx.table.root(
-                        rx.table.body(
-                            rx.foreach(
-                                data[0],  # TODO: function to change whose data shows up
-                                patient_table,
+                    rx.hstack(
+                        rx.box(
+                            rx.input(
+                                name="organ",
+                                variant="surface",
+                                size="2",
+                                disabled=rx.cond(
+                                    RegisterState.organ_inputted == True, True, False
+                                ),
+                                cursor=rx.cond(
+                                    RegisterState.organ_inputted == True,
+                                    "not-allowed",
+                                    "auto",
+                                ),
+                            ),
+                            width="50%",
+                        ),
+                        rx.button(
+                            "Submit",
+                            on_click=RegisterState.input_organ,
+                            disabled=rx.cond(
+                                RegisterState.organ_inputted == True, True, False
                             ),
                         ),
-                        width="50%",
+                    ),
+                    rx.cond(
+                        RegisterState.organ_inputted,
+                        rx.box(
+                            rx.text(
+                                "Valid organ has been requested. The following information will be uploaded to our secure blockchain network.",
+                                margin_top="24px",
+                                margin_bottom="12px",
+                            ),
+                            rx.center(
+                                rx.table.root(
+                                    rx.table.body(
+                                        rx.foreach(
+                                            data[
+                                                0
+                                            ],  # TODO: function to change whose data shows up
+                                            patient_table,
+                                        ),
+                                        rx.table.row(
+                                            rx.table.column_header_cell("Organ Needed"),
+                                            rx.table.cell("Kidney"),
+                                        ),
+                                    ),
+                                    width="50%",
+                                ),
+                            ),
+                            rx.text(
+                                "Please review patient vitals before proceeding.",
+                                margin_top="24px",
+                                margin_bottom="12px",
+                            ),
+                            rx.chakra.stat(
+                                rx.chakra.stat_label("Example Stat"),
+                                rx.chakra.stat_number("100 bpm"),
+                                rx.chakra.stat_help_text("Description"),
+                            ),
+                        ),
                     ),
                 ),
             ),
+            type="scroll",
+            scrollbars="vertical",
         )
     )
